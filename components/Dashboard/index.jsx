0c9194bc31d551b2fa39436e3999e572081d3ae3
maxLambdadev <berishvilitemoraz11@gmail.com>
2024-03-15 13:13:48 -0400

    add signout btn

--- components/Dashboard/index.jsx
+++ components/Dashboard/index.jsx
@@ -2,13 +2,16 @@
 
 import React, { useState, useEffect } from "react"
 import { useRouter } from 'next/navigation';
+import { signOut } from 'next-auth/react';
 
 export default function Dashboard(props) {
     const router = useRouter();
     
     return (
-        <div className="dashboard-wra">
-            Dashboard
+        <div className="dashboard-wra">            
+            <button aria-label="" className="btn btn-primary btn-lg m-t-10" type="submit" onClick={signOut}>
+                Sign out
+            </button>
         </div>
     )
 }
